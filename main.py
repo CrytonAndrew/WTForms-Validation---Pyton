d43013891993253231b674fb22319b654208bb6a
Cryton Andrew <crytonadrew21@gmail.com>
2021-03-24 06:11:21 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+import email_validator
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label="Submit")
+
+
+app = Flask(__name__)
+app.secret_key = "lksjldkfjasfijweplfjdplfdsflksdjfklsjkl"
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email" and login_form.password.data == "123456":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template('login.html', form=login_form)
+
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
